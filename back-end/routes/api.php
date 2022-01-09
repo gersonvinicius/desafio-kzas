4d596b8827cb5b67c912fef97168b9d0f364e8da
gersu <vini_gerson@hotmail.com>
2022-01-09 02:16:21 -0300

    criada api para autenticacao

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -17,3 +17,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// ROUTES LOGIN API
+Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+//API route for login user
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/user', function(Request $request) {
+        // return auth()->user();
+        return response()
+            ->json(['user' => auth()->user()]);
+    });
+    // API route for logout user
+    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+
+    // Route::put('/salvar-dados', [App\Http\Controllers\Api\ClientController::class, 'update']);
+    // Route::resource('/client',App\Http\Controllers\Api\ClientController::class);
+    // Route::post('/client/save-files','App\Http\Controllers\Api\ClientController@saveFiles');
+    // Route::post('/client/remove-files','App\Http\Controllers\Api\ClientController@removeFiles');
+
+
+});
